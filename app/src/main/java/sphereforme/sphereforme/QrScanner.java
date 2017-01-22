f4932172534b0e1e5c33fb4b1c03366250761f9b
Stormys <julian.tuminaro@gmail.com>
2017-01-21 18:55:54 -0800

    Basic Qr scanning

--- app/src/main/java/sphereforme/sphereforme/QrScanner.java
+++ app/src/main/java/sphereforme/sphereforme/QrScanner.java
@@ -1,34 +1,66 @@
 package sphereforme.sphereforme;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import java.io.IOException;
+
+import static android.content.pm.PackageManager.PERMISSION_DENIED;
+
 public class QrScanner extends AppCompatActivity {
+
+    private final int Camera_Permissions = 2;
+
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private TextView barcodeInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_scanner);
 
-        SurfaceView  cameraView = (SurfaceView)findViewById(R.id.camera_view);
+        cameraView = (SurfaceView)findViewById(R.id.camera_view);
+        barcodeInfo = (TextView)findViewById(R.id.barcodeInfo);
 
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
 
-        CameraSource cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(640, 480).build();
-/*
+        cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(1600, 1024).build();
+
+        set_up_camera_with_surface_view();
+        set_up_barcode_dectector();
+    }
+
+    private void set_up_camera_with_surface_view() {
         cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 try {
-                    cameraSource.start(cameraView.getHolder());
+                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(cameraView.getHolder());
+                    } else {
+                        requestCameraPermission();
+                    }
                 } catch (IOException ie) {
-                    Log.e("CAMERA SOURCE", ie.getMessage());
                 }
             }
 
@@ -38,7 +70,62 @@ public class QrScanner extends AppCompatActivity {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
             }
-        });*/
+        });
+    }
+
+    private void set_up_barcode_dectector() {
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void requestCameraPermission() {
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+        ActivityCompat.requestPermissions(QrScanner.this, permissions, Camera_Permissions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults[0] == PERMISSION_DENIED) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            finish();
+        } else {
+            Start_Camera();
+        }
+    }
+
+    private void Start_Camera() {
+        try {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                cameraSource.start(cameraView.getHolder());
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraSource.release();
+        barcodeDetector.release();
     }
 }
