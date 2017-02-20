11242a66a2f40ce269cdeb60abe345c5223aa437
Stormys <julian.tuminaro@gmail.com>
2017-02-19 18:33:04 -0800

    ContactBook startup

--- app/src/main/java/sphereforme/sphereforme/Activities/ContactBook.java
+++ app/src/main/java/sphereforme/sphereforme/Activities/ContactBook.java
@@ -2,7 +2,12 @@ package sphereforme.sphereforme.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import org.json.JSONArray;
+
+import sphereforme.sphereforme.Network.AsyncTaskCompleteListener;
+import sphereforme.sphereforme.Network.NetworkManager;
 import sphereforme.sphereforme.R;
 
 public class ContactBook extends BaseActivity {
@@ -11,5 +16,31 @@ public class ContactBook extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_book);
+        new Get_Contact_Book().launchTask("my_contact_book","");
     }
+
+
+    private class Get_Contact_Book implements AsyncTaskCompleteListener<String> {
+
+        @Override
+        public void onTaskComplete(String result) {
+            JSONArray json_result = null;
+            try {
+                json_result = new JSONArray(result);
+            }catch(Exception e){
+
+            }
+            ContactBookAdapter adapter = new ContactBookAdapter(ContactBook.this, json_result);
+
+            ListView listView = (ListView) findViewById(R.id.table);
+            listView.setAdapter(adapter);
+        }
+
+        @Override
+        public void launchTask(String url, String urlParameters) {
+            NetworkManager NetworkConnection = new NetworkManager(this);
+            NetworkConnection.execute(url, urlParameters);
+        }
+    }
+
 }
